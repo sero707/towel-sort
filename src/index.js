5e9c938fc4fd3aa6d81cdddf7c66daf572798254
Sergey Nedashkovskiy <ser.nedash@gmail.com>
2023-01-31 19:50:20 +0300

    feat: add function

--- src/index.js
+++ src/index.js
@@ -2,5 +2,15 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
-}
+	if(matrix == undefined){
+		return [];
+	} else{
+	let arr = matrix;
+	arr.forEach((element,index) => {
+		if(index % 2 === 1){
+			element.reverse();
+		};
+	});
+	return arr.flat(Infinity)
+	};
+};
